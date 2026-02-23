bfe2f7ab6bf64ca9f7513a9b3fc0b1b6647a0644
aleenacb <aleenacbenoy@gmail.com>
2026-02-23 20:04:35 +0530

    Add files via upload

---
+++ DSA Java/Bitwise.java
@@ -0,0 +1,16 @@
+class Bitwise{
+    public static boolean isBitwise(int n) {
+        if((n & 1) == 0)
+            return true;
+            else 
+            return false;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        if(isBitwise(n) == true) 
+            System.out.println("true");
+            else
+            System.out.println("false");
+        
+    }
+}
