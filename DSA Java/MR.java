3df79e4db0894dad9ab5390a0b44eba21bce47a0
aleenacb <aleenacbenoy@gmail.com>
2026-02-27 20:53:19 +0530

    Add files via upload

---
+++ DSA Java/MR.java
@@ -0,0 +1,17 @@
+//Multiplication recursive approach
+class MR {
+    static void printTable(int n, Integer...val) {
+        int i = 1;
+        if(val.length != 0)
+        i = val[0];
+        if(i == 11)
+        return;
+        System.out.println(n * i);
+        i++;
+        printTable(n, i);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printTable(n);
+    }
+}
---
+++ DSA Java/Multiplication.java
@@ -0,0 +1,10 @@
+class Multiplication {
+    public static void printTable (int n){
+        for(int i = 0; i <= 10; ++i) {
+        System.out.println(n * i);
+        }
+    }
+        public static void main(String[] args) {
+            printTable(5);
+        }
+    }
