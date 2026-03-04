49b358ff18c4b594bd120d96814baf97f19c9c04
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-04 23:34:51 +0530

    Create Max.java

---
+++ OOPs/Max.java
@@ -0,0 +1,18 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 20};
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                second = largest;
+                largest = num;
+            } else if (num > second && num != largest) {
+                second = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + second);
+    }
+}
