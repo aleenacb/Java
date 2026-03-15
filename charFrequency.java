06ef500efc407dc15e5f90c1acf4fd35626d693e
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-15 23:54:22 +0530

    Create charFrequency.java

---
+++ charFrequency.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "hello";
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(char c : str.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+
+        System.out.println(map);
+    }
+}
