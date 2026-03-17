6c1ece6d8f425007ac6e9826a721486226db72ff
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-17 22:34:20 +0530

    Add files via upload

---
+++ ExceptionHandling/trycEx1.java
@@ -0,0 +1,16 @@
+import java.util.NullPointerException;
+import java.ArrayIndexOutOfBoundException;
+public class trycEx1 {
+    public static void main(String[] args) {
+        try{
+            int[] arr = new int[5];
+            arr[10] = 1;
+        } catch (NullPointerException e) {
+            System.out.println("Null Pointer");
+        } catch (ArrayIndexOutOfBoundException e) {
+            System.out.println("Array out of bounds");
+        } finally {
+            System.out.println("Done");
+        }
+    }
+}
