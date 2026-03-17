5ee7b9ee0e934c8c3f67cca531178017eaa76e01
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-17 22:31:13 +0530

    createExceptionHandling

---
+++ ExceptionHandling/trycEx.java
@@ -0,0 +1,10 @@
+public class trycEx {
+    public static void main(String[] args) {
+        try{
+            System.out.println("try");
+            return;
+        } finally {
+            System.out.println("finally");
+        }
+    }
+}
