fe032f6997caae0420ea30734a95a863e40575e0
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-21 23:56:45 +0530

    Add SimpleInterest class to calculate interest

---
+++ SimpleInterest.java
@@ -0,0 +1,7 @@
+class SimpleInterest {
+  public static void main(String[] args){
+    float p = 1, r = 1, t = 1;
+    float si = (p * t * r)/100;
+    System.out.println("Simple interest" + si);
+  }
+}
