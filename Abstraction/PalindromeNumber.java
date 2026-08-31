82c1c3ce6601218d370a599e01570e0fe42f18c3
TechWithAleenaCB <184249015+aleenacb@users.noreply.github.com>
2026-08-31 21:28:53 +0530

    Add files via upload

---
+++ Abstraction/PalindromeNumber.java
@@ -0,0 +1,15 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int n = 121;
+        int original = n;
+        int reverse = 0;
+        while ( n != 0) {
+            reverse = reverse * 10 + n % 10;
+            n = n / 10;
+        }
+        if (original == reverse) 
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
