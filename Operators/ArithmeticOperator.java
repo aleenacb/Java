c3884527a16d475736a57252b4688e3458a0e5de
TechWithAleenaCB <184249015+aleenacb@users.noreply.github.com>
2026-09-18 10:14:55 +0530

    Add ArithmeticOperator class for basic operations

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,22 @@
+package Operators;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+        //Addition
+        int sum = a + b;
+        //Subtraction
+        int sub = a - b;
+        //Multiplication
+        int mul = a * b;
+        //Divide
+        int div = a / b;
+        //Modulus
+        int mod = a % b;
+        System.out.println("Addition : " + sum);
+        System.out.println("Subtraction : " + sub);
+        System.out.println("Multiplication : " + mul);
+        System.out.println("Division : " + div);
+        System.out.println("Modulus : " + mod);
+    }
+}
