e67ce5c66f0e221ce9872e9c773c731a1fa9f74f
TechWithAleenaCB <184249015+aleenacb@users.noreply.github.com>
2026-09-18 10:16:29 +0530

    Add IfStatement class with basic condition check

---
+++ DecisionMaking/IfStatement.java
@@ -0,0 +1,9 @@
+package DecisionMaking;
+public class ifStatement {
+    public static void main(String[] args) {
+        int i = 10;
+        if (i < 15) {
+            System.out.println("Condition is true");
+        }
+    }
+}
